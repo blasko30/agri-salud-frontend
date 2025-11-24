bc1f58693cbc91a50f0e5a7c0b6fcd30fdbc52c4
blasko30 <blasko1312@gmail.com>
2025-11-24 13:38:11 -0300

    arreglo de ajustes de carpetas automaticas

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7obeq39e3ic"
+    "revision": "0.4edgjng372"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
